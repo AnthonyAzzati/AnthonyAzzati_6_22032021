6466cba1edacb09150897658bed559468d32fc43
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-04-04 10:21:23 +0200

    Permet de masquer une partie du mail par des "***"

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const bcrypt = require("bcrypt");
-const jwb = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const maskData = require("maskdata");
 const passwordValidator = require("password-validator");
 
 const User = require("../models/User");
@@ -31,7 +32,7 @@ exports.signup = (req, res, next) => {
     .hash(req.body.password, 10)
     .then((hash) => {
       const user = new User({
-        email: req.body.email,
+        email: maskData.maskEmail2(req.body.email),
         password: hash,
       });
       if (checkPassword.validate(req.body.password)) {
@@ -48,7 +49,7 @@ exports.signup = (req, res, next) => {
 
 // middleware de connexion
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  User.findOne({ email: maskData.maskEmail2(req.body.email) })
     .then((user) => {
       if (!user) {
         return res.status(401).json({ error: "Utilisateur non trouvé" });
@@ -61,7 +62,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwb.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
               expiresIn: "24h",
             }),
           });
