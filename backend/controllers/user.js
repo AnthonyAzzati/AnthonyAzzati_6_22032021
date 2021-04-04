fbc53a9552a6d82425b9cecd346c4a6a401da604
Anthony Azzati <anthonyazzati.dev@gmail.com>
2021-04-04 10:06:29 +0200

    Remplacement des ' par des "

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const bcrypt = require('bcrypt');
-const jwb = require('jsonwebtoken');
-const passwordValidator = require('password-validator');
+const bcrypt = require("bcrypt");
+const jwb = require("jsonwebtoken");
+const passwordValidator = require("password-validator");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 const checkPassword = new passwordValidator();
 
@@ -37,7 +37,7 @@ exports.signup = (req, res, next) => {
       if (checkPassword.validate(req.body.password)) {
         user
           .save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
+          .then(() => res.status(201).json({ message: "Utilisateur créé" }))
           .catch((error) => res.status(400).json({ error }));
       } else {
         throw new Error();
@@ -51,18 +51,18 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé' });
+        return res.status(401).json({ error: "Utilisateur non trouvé" });
       }
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect' });
+            return res.status(401).json({ error: "Mot de passe incorrect" });
           }
           res.status(200).json({
             userId: user._id,
-            token: jwb.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
-              expiresIn: '24h',
+            token: jwb.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
             }),
           });
         })
